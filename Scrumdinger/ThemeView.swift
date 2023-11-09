6c1018d08a8337456f25830b04bb92b645fc26c7
GabrielEduardoSilva <gaebds@gmail.com>
2023-11-09 09:46:00 -0300

    DetailEditView Created

---
+++ Scrumdinger/ThemeView.swift
@@ -0,0 +1,19 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by Gabriel Eduardo on 08/11/23.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    let theme: Theme
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThemeView()
+}
